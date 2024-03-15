e12018517c2c054ffcc0e5e5619f51680f968bf8
subu53 <subusam5@gmail.com>
2024-03-15 20:57:02 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,17 +1,27 @@
 #include <iostream>
-
 using namespace std;
-
 int main()
 {
-    int age;
-    cout << "Student, Enter your age" << endl;
-    cin >> age;
-    if (age > 20 && age<25)
-    {
-        cout << "You're an Adult!" << endl;
-    } else {
-        cout << "That age has not been considered" << endl;
-    }
+    double currency, USD, CYN, EUR, GBP, KSH, AMOUNT;
+ cout <<"Enter The Name Currency In Capital: e.g USD, CYN, EUR, GBP " ;
+ cin >> currency;
+ cout <<"Enter The Amount in figures(Without spaces or commas): ";
+  cin >> AMOUNT;
+    if (currency ==USD) {
+        KSH = AMOUNT*152 ;
+        cout<< "The Amount in Kshs is: "<< KSH ;
+    	}
+    if (currency ==CYN) {
+        KSH = AMOUNT*21.27 ;
+        cout<< "The Amount in Kshs is: "<< KSH ;
+        }
+    if (currency ==EUR) {
+        KSH = AMOUNT*166.88 ;
+        cout<< "The Amount in Kshs is: "<< KSH ;
+        }
+    else (currency ==GBP); {
+        KSH = AMOUNT*190.73 ;
+        cout<< "The Amount in Kshs is: "<< KSH ;
+        }
     return 0;
-}
+    }
